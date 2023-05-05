23bf07fefee6d6db229b48deeb03379e3fb9a078
C. Large <contact@chantzlarge.com>
2023-05-05 13:19:51 -0400

    Improved tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,16 @@
 import { completion } from './index'
 
 describe('completion', () => {
-    test('SHOULD create completion', async () => {
+    test('SHOULD create completion without arguments', async () => {
         const got = await completion`Please echo the following to prove we can communicate: "hello"`
         console.log(got)
-    })
-    test('SHOULD create completion', async () => {
-        const got = await completion`${""}`
+    }, 6000)
+    test('SHOULD create completion with one argument', async () => {
+        const got = await completion`Please echo the following to prove we can communicate: ${"hello"}`
         console.log(got)
-    })
+    }, 6000)
+    test('SHOULD create completion with multiple arguments', async () => {
+        const got = await completion`Please echo the following to prove we can communicate: ${"hello"}, ${"world"}!`
+        console.log(got)
+    }, 6000)
 })
